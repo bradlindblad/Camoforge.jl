426c6b19b9404f5c0e34b1512d4efdb46245cfdd
bradlindblad <bradley.lindblad@gmail.com>
2020-12-17 09:21:51 -0600

    created gh-pages

---
+++ docs/build/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"index.html","page":"Home","title":"Home","text":"CurrentModule = Camoforge","category":"page"},{"location":"index.html#Camoforge","page":"Home","title":"Camoforge","text":"","category":"section"},{"location":"index.html","page":"Home","title":"Home","text":"","category":"page"},{"location":"index.html","page":"Home","title":"Home","text":"Modules = [Camoforge]","category":"page"}]
+}
