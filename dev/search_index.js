fc4c5ab6e43feffacfe5570b997e571c4e1ed11b
Documenter.jl <documenter@juliadocs.github.io>
2020-12-17 15:32:01 +0000

    build based on 8ab9f08

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Camoforge","category":"page"},{"location":"#Camoforge","page":"Home","title":"Camoforge","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Camoforge]","category":"page"}]
+}
